4a0ad7973c0e387eab4964d41fc18ad75d2c6542
Evan Su <evan.pohsun.su@gmail.com>
2016-04-13 21:29:06 +0800

    Add testing and move argument report

--- dql.lua
+++ dql.lua
@@ -6,9 +6,7 @@ Inputs for initialization:
   env: environment
   param: parameters of the learning process
     step: Max number of steps of each episode
-    capacity: capacity of replay memory
-    batchsize: batch size of samples from replay memory
-    discount: discount in cumulative reward
+    
   
     
   statePreprop: a function preprocess observation from environment to state (optional)
@@ -23,41 +21,43 @@ methods:
       report: a function called after each episode (optional)
     output:
       dqn: learned dqn
+  testing:
+    inputs:
+      visualization: a function visualizes the state of each step
 
 ]]--
 
+require 'xlua'
 local classic = require 'classic'
+require 'classic.torch' -- for saving/loading using torch.save/torch.load
 
 local dql = classic.class('dql')
 
-function dql:_init(dqn, env, param, statePreprop, actPreprop, report)
+function dql:_init(dqn, env, param, statePreprop, actPreprop)
   self.dqn = dqn
   self.env = env
   self.param = param
-  print('self.param')
-  rPrint(self.param)
   self.statePreprop = statePreprop or function(observation) return observation end
   self.actPreprop = actPreprop or function (act) return act end
 end
 
 function dql:learning(episode, report)
-  local report = report or function(...) return ... end
   local updateCounter = 0
   for e = 1, episode do
     -- initialize state
     local observation = self.env:start()
     local state = self.statePreprop(observation)
-    print('init state', state)
+    --print('init state', state)
     for t = 1, self.param.step do 
       local action = self.dqn:act(state)
       local actionProp = self.actPreprop(action)
-      print('action', action)
-      print('actionProp', actionProp)
+      --print('action', action)
+      --print('actionProp', actionProp)
       local reward, observation, terminal = self.env:step(actionProp)
-      print('reward', reward)
-      print('terminal', terminal)
+      --print('reward', reward)
+      --print('terminal', terminal)
       local nextState = self.statePreprop(observation) -- assume fully observable
-      print('nextState', nextState)
+      --print('nextState', nextState)
       
       local trans = {s = state:clone(), a = action:clone(), r = reward,
                      ns = nextState:clone(), t = terminal}
@@ -73,9 +73,31 @@ function dql:learning(episode, report)
       if terminal then
         break
       end
-    end 
+    end
+    if report then report(self) end
   end
   return self.dqn
+end
+
+function dql:test(episode, visualization)
+  for e = 1, episode do
+    -- initialize state
+    local observation = self.env:start()
+    local state = self.statePreprop(observation)
+    for t = 1, self.param.step do
+      if visualization then visualization(state) end
+      local action = self.dqn:act(state)
+      local actionProp = self.actPreprop(action)
+      local reward, observation, terminal = self.env:step(actionProp)
+      local nextState = self.statePreprop(observation) -- assume fully observable
+      
+      -- end of step
+      state = nextState -- update state
+      if terminal then
+        break
+      end
+    end
+  end
 end
 
 return dql
