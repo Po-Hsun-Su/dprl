147f5862b19b2e2c3f779f1d62a346ded76efeba
Evan Su <evan.pohsun.su@gmail.com>
2016-05-22 18:30:24 +0800

    Add bdqn bdql Bootstrap

--- init.lua
+++ init.lua
@@ -1,9 +1,12 @@
 require 'torch'
-
+require 'nn'
 local dprl = {}
 
 dprl.dqn = require 'dqn'
 dprl.ddqn = require 'ddqn'
 dprl.dql = require 'dql'
+require 'Bootstrap'
+dprl.bdqn = require 'bdqn'
+dprl.bdql = require 'bdql'
 
 return dprl
