78a73878aa13747692dcc74cbb7749bb2220ce17
Evan Su <evan.pohsun.su@gmail.com>
2016-05-23 11:46:36 +0800

    Learning at the end of an episode not within an episode. Learning within an episode may disrupt deep exploration.

--- bdql.lua
+++ bdql.lua
@@ -14,6 +14,7 @@ end
 
 function bdql:learning(episode, report)
   local updateCounter = 0
+  
   for e = 1, episode do
     -- Sample index of active head
     local active = math.random(self.headNum)
@@ -26,6 +27,7 @@ function bdql:learning(episode, report)
     local Qvalues = {}
     states[1] = observation
     --print('init state', state)
+    local stepsTaken = 0
     for t = 1, self.config.step do 
       local action = self.dqn:act(state, active)
       local actionProp = self.actPreprop(action)
@@ -42,19 +44,34 @@ function bdql:learning(episode, report)
       
       local trans = {s = state:clone(), a = action:clone(), r = reward,
                      ns = nextState:clone(), t = terminal}
-      local sampleTrans = self.dqn:replay(trans)
+      self.dqn:store(trans)
+      --[[
+      local sampleTrans = self.dqn:sample()
       self.dqn:learn(sampleTrans)
       
       updateCounter = updateCounter + 1
       if updateCounter%self.config.updatePeriod == 0 then
         self.dqn:update()
       end
+      ]]--
       -- end of step
       state = nextState -- update state
       if terminal then
+        stepsTaken = t
         break
       end
     end
+    
+    -- learning
+    for t = 1, stepsTaken do
+      self.dqn:learn(self.dqn:sample())
+      
+      updateCounter = updateCounter + 1
+      if updateCounter%self.config.updatePeriod == 0 then
+        self.dqn:update()
+      end
+    end
+    
     if report then report(self, totalReward, active,  states, Qvalues) end
   end
   return self.dqn
