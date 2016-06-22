a8a56832eba1f429b5f9647f9b3c199db3831664
Evan Su <evan.pohsun.su@gmail.com>
2016-06-22 16:57:55 +0800

    Fix major bugs in aac. 1. Add missing zeroGradParameter call. 2. Override the faulty action sampled action in accGradParameters with saved action in memory.

--- aac.lua
+++ aac.lua
@@ -5,6 +5,8 @@ require 'optim'
 local optimInit = require 'optimInit'
 
 function aac:_init(anet, cnet, config, optim, optimConfig)
+  assert(torch.isTypeOf(anet.modules[#anet.modules],nn.Reinforce), 'The last module of acter network must be a subclass of nn.Reinforce')
+  
   self.anet = anet:clone()
   self.cnet = cnet:clone()
   self.actorParameters, self.actorGradParameters = self.anet:getParameters()
@@ -26,6 +28,18 @@ function aac:_init(anet, cnet, config, optim, optimConfig)
   end
 end
 
+function aac:training()
+  self.anet:training()
+  self.cnet:training()
+end
+
+
+function aac:evaluate()
+  self.anet:evaluate()
+  self.cnet:evaluate()
+end
+
+
 function aac:getParameters()
   return {self.actorParameters, self.criticParameters}
 end
@@ -41,8 +55,6 @@ end
 
 function aac:sync(sharedParameters)
   local sharedActorParameters, sharedCriticParameters = unpack(sharedParameters)
-  --print('sharedActorParameters',sharedActorParameters)
-  --print('sharedCriticParameters', sharedCriticParameters)
   self.actorParameters:copy(sharedActorParameters)
   self.criticParameters:copy(sharedCriticParameters)
 end
@@ -70,11 +82,12 @@ end
 
 -- rewrite this function. Write a3c first
 function aac:accGradParameters(nextState, terminal)
+  self:zeroGradParameters()
   assert(not self.memory.full, 'Number of stored transitions should not exceed tmax')
   local tend = self.memory.index
   local mbState = self.memory.storage.s[{{1,tend},{}}]
   local r = self.memory.storage.r[{{1,tend}}]
-  
+  local mbAction = self.memory.storage.a[{{1,tend}}]
   -- decide initial return (R)
   local lastStateReturn = self:getReturn(nextState, terminal)
   local R = torch.Tensor():typeAs(self.memory.storage.r):resize(tend)
@@ -85,28 +98,31 @@ function aac:accGradParameters(nextState, terminal)
   for t = tend-1,1, -1 do
     R[t] = r[t] + gamma*R[t+1]
   end
-  --print('R',R)
-  --print('r',r)
-  
+
   -- accumulate gradient in minibatch
-  -- actor
-  local A = self.anet:forward(mbState)
-  self.fa = R
-  self.anet:reinforce(R)
-  -- the last layer of anet must be a REINFORCE module in dpnn
-  -- Can support other critirion for mixing supervised learning
-  local dA_do = torch.Tensor():typeAs(A):resizeAs(A):zero() 
-  self.anet:backward(mbState, dA_do)
   -- critic
   local V = self.cnet:forward(mbState) -- evaluate value of all states in memory
   self.fc = self.cnetCriterion:forward(V, R)
   local dfc_do = self.cnetCriterion:backward(V, R)
   self.cnet:backward(mbState, dfc_do)
   
+  -- actor
+  -- Shouldn't sample action in forward again. The action sampled here must be the same as the one getting the reward.
+  -- Need to feed the actions in memory to anet. i.e. A:copy(mbAction:viewAs(A))
+  local A = self.anet:forward(mbState)
+  A:copy(mbAction:viewAs(A)) -- override newly sampled action with stored action. Need a less hacky way to do this
+
+  self.fa = R-V
+  self.anet:reinforce(R-V)
+  -- the last layer of anet must be a REINFORCE module in dpnn
+  -- Can support other critirion for mixing supervised learning
+  local dA_do = torch.Tensor():typeAs(A):resizeAs(A):zero() -- dummy gradient. REINFORCE module ignores it.
+  self.anet:backward(mbState, dA_do)
+  
   -- reset memory
   self.memory:reset()
   
-  return self.actorGradParameters, self.criticGradParameters
+  return {self.actorGradParameters, self.criticGradParameters}
 end
 
 function aac:update(sharedParameters, T, t, sharedOptimState)
