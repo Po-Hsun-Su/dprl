752053bc6cb47dea7012a9bec134fec2de2767aa
Evan Su <evan.pohsun.su@gmail.com>
2016-06-22 16:59:02 +0800

    Add nn module doing entroy regularization

---
+++ EntropyRegularization.lua
@@ -0,0 +1,34 @@
+--[[
+Entropy regularization for policy gradient.
+This module should be added after softmax module and before 
+ReinforceCategorical module.
+
+This module has no effect in the forward pass.
+However, in the backward pass, this module add gradient that increases
+the entropy of the probability distribution of actions to the gradInput
+from the ReinforceCategorical module.
+
+On construction, a parameter "beta" specifying the strength of regularization
+ is required.
+
+]]--
+
+local EntropyRegularization, parent = torch.class('nn.EntropyRegularization', 'nn.Module')
+
+function EntropyRegularization:__init(beta)
+  self.beta = beta
+end
+
+function EntropyRegularization:updateOutput(input)
+  self.output = input
+  return self.output
+end
+
+function EntropyRegularization:updateGradInput(input, gradOutput)
+  local dE_da = (torch.log(input) + 1)
+  --print('torch.log(input)', torch.log(input))
+  --print('dE_da',dE_da)
+  self.gradInput =  gradOutput + self.beta*dE_da
+  --print('self.gradInput',self.gradInput)
+  return self.gradInput
+end
