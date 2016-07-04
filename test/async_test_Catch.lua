565f66e685bf785e421be51515c29a665f0dae26
Evan Su <evan.pohsun.su@gmail.com>
2016-07-04 22:21:47 +0800

    Update test script

--- test/async_test_Catch.lua
+++ test/async_test_Catch.lua
@@ -1,3 +1,9 @@
+-- cmd options 
+local cmd = torch.CmdLine()
+cmd:option('-threads', 4, 'number of async agent thread')
+cmd:option('-load', '', 'load saved parameters')
+local opts = cmd:parse(arg)
+
 require 'nn'
 require 'dpnn'
 require 'rPrint'
@@ -34,7 +40,7 @@ anet:add(nn.ReLU(true))
 anet:add(nn.View(-1):setNumInputDims(3))
 
 local convOutputSize = anet:forward(torch.Tensor(1,stateSpec[2][1],stateSpec[2][2],stateSpec[2][3])):size():totable()
-local hiddenSize = 128
+local hiddenSize = 256
 anet:add(nn.Linear(torch.prod(torch.Tensor(convOutputSize)), hiddenSize))
 anet:add(nn.ReLU(true))
 -- create critic net sharing parameters with action net 
@@ -54,12 +60,22 @@ local optimConfig = {learningRate = 0.001,
                      momentum = 0.9}
 local aac = dprl.aac(anet, cnet, config, optimMethod,optimConfig)
 
+-- load parameters if available
+if opts.load ~='' then
+  local parameters = torch.load(opts.load)
+  aac:setParameters(parameters)
+  print('Load parameter', opts.load)
+end
+
+
+
 -- initialize async
 local loadPackage = function(threadIdx) -- load package on creating agent threads
   require 'rPrint'
   require 'rPrintModule'
   require 'EntropyRegularization'
   require 'xlua'
+  require 'rlenvs'
   -- mask rPrint except thread 1
   if threadIdx ~= 1 then
     rPrint = function () end
@@ -67,11 +83,11 @@ local loadPackage = function(threadIdx) -- load package on creating agent thread
   end
 end
 -- set config of async
-local asyncConfig = {nthread = 4, loadPackage = loadPackage, maxSteps = 10000}
+local asyncConfig = {nthread = opts.threads, loadPackage = loadPackage, maxSteps = 10000}
 
 -- set preprocessing functions for state and action
 local observationHist 
-local preprop = function (observation)
+local statePreprop = function (observation)
   if not observationHist then
     observationHist = torch.Tensor():typeAs(observation):resize(observation:size()):zero()
   end
@@ -84,24 +100,23 @@ end
 local oneHot2Index = torch.linspace(actionSpec[3][1], actionSpec[3][2], actionRange)
 local actionPreprop =  function(action)
   local act = action:view(-1)*oneHot2Index
-  -- act must be number
   return act
 end
 
 -- setup a3c
 local a3c = dprl.asyncl(aac, env, asyncConfig, statePreprop, actionPreprop)
 
-local epoch = 200
+local epoch = 300
 local stepsPerEpoch = 50000
 local testEpisode = 1000
 -- set learning report called in each step
 local totalReward = 0
 local reportT = 0
-local learningReport = function(T, trans, agent)
+local learningReport = function(trans, t, T)
   if __threadid == 1 then
-    if T:get() - reportT> 1000 then
-      xlua.progress(T:get(), stepsPerEpoch)
-      reportT = T:get()
+    if T - reportT> 1000 then
+      xlua.progress(T, stepsPerEpoch)
+      reportT = T
     end
     totalReward = totalReward + trans.r
     if trans.t then
@@ -113,22 +128,39 @@ end
 
 --set test report called in each step
 local testtotalReward = 0
-local averageTotalReward = 0
-local testReport = function(s, a, r, ns, t)
-  testtotalReward = testtotalReward + r
+local testStepReport = function(trans, t, E)
+  testtotalReward = testtotalReward + trans.r
+  if __threadid == 1 then
+    xlua.progress(E, testEpisode)
+  end
+  local totalRewardCP = testtotalReward
   if t then
-    averageTotalReward = averageTotalReward + testtotalReward
+    --print(testtotalReward)
     testtotalReward = 0
   end
+  return totalRewardCP
 end
 
+local averageTotalReward = 0
+local testEpisodicReport = function(report, e)
+  averageTotalReward = averageTotalReward + report
+end
+
+local bestAverageTotalReward= 0
 for i = 1, epoch do
   print('Learning epoch', i)
   a3c:learn(stepsPerEpoch, learningReport)
   print('testing')
-  a3c:test(testEpisode,testReport)
-  print('Average total reward')
-  print(averageTotalReward/testEpisode)
+  a3c:test(testEpisode,testStepReport, testEpisodicReport)
+  print('averageTotalReward', averageTotalReward/testEpisode)
+  averageTotalReward = 0
+  
+  if  averageTotalReward > bestAverageTotalReward then
+    local sharedParameters = aac:getParameters()
+    print('Saving parameters')
+    torch.save('a3cAtari' .. date .. '.t7',sharedParameters)
+    bestAverageTotalReward = averageTotalReward
+  end
   averageTotalReward = 0
 end
 
