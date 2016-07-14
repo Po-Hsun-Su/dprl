d6aff63a38cf23c95e9eaff93cbc81a568193226
Evan Su <evan.pohsun.su@gmail.com>
2016-07-14 17:54:10 +0800

    Correct which tensor to share

--- dprl/optimInit.lua
+++ dprl/optimInit.lua
@@ -2,14 +2,17 @@
 assert(optim, 'Load optim package before using optimInit')
 
 local optimInit = function(optimMethod, x, dfdx)
+  print('Initializing shared optimization state')
   local state = {}
   if optimMethod == optim.rmsprop then
-    state.m = torch.Tensor():typeAs(x):resizeAs(dfdx):fill(1)
-    state.tmp = torch.Tensor():typeAs(x):resizeAs(dfdx)
+    state.g = torch.Tensor():typeAs(x):resizeAs(dfdx):fill(1)
+  elseif optimMethod == optim.rmspropm then
+    state.g = torch.Tensor():typeAs(x):resizeAs(dfdx):zero()
+    state.gSq = torch.Tensor():typeAs(x):resizeAs(dfdx):zero()
   else
     print('WARNING: no initialization scheme found for this optim method')
   end
   return state
 end
 
-return optimInit 
+return optimInit
